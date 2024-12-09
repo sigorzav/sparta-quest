5b71996f0485ad12679f5c32d53ecb767f25febf
sigorzav <124122137+sigorzav@users.noreply.github.com>
2024-12-09 21:00:45 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.py
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.py
@@ -2,6 +2,6 @@ def solution(num1, num2):
     answer = 0
     
     if all((0 <= i <= 100) for i in [num1, num2]):
-        answer = (int)((num1 / num2) * 1000)
+        answer = int(num1 / num2 * 1000)
     
     return answer
