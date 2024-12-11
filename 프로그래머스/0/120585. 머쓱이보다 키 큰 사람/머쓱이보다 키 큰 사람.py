24971992393be66aa41aa01687d747165edb13db
sigorzav <124122137+sigorzav@users.noreply.github.com>
2024-12-11 10:38:22 +0900

    Update 머쓱이보다 키 큰 사람.py

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -1,11 +1,2 @@
 def solution(array, height):
-    answer = 0
-    
-    if 1 <= len(array) <= 100 and 1 <= height <= 200:
-        array.sort()
-
-        for i in array:
-            if 1 <= i <= 200 and height < i:
-                answer += 1
-                
-    return answer
+    return sum(1 for i in array if i > height)
