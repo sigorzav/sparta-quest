addc796fc13cf7a988cc5b06a023139266e77399
sigorzav <124122137+sigorzav@users.noreply.github.com>
2024-12-11 22:13:28 +0900

    Update 배열 원소의 길이.py

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
@@ -1,7 +1,13 @@
+# sigorzav
 def solution(strlist):
     answer = []
     
     for i in strlist:
        answer.append(len(i)) 
     
+    return answer
+
+# learn
+def solution(strlist):
+    answer = [len(i) for i in strlist]
     return answer
