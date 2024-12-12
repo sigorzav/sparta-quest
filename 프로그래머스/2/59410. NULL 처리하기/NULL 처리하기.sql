08651b4a9dc16ee991585c17be0c6d716f8bdcae
sigorzav <124122137+sigorzav@users.noreply.github.com>
2024-12-12 11:06:12 +0900

    Update NULL 처리하기.sql

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,8 +1,23 @@
+1) CASE문 사용
     SELECT ANIMAL_TYPE
          , CASE 
             WHEN NAME IS NULL THEN 'No name'
             ELSE NAME
            END AS NAME
+         , SEX_UPON_INTAKE
+      FROM ANIMAL_INS 
+  ORDER BY ANIMAL_ID      
+
+2) IFNULL문 사용    
+    SELECT ANIMAL_TYPE
+         , IFNULL(NAME, 'No name') AS NAME
+         , SEX_UPON_INTAKE
+      FROM ANIMAL_INS 
+  ORDER BY ANIMAL_ID
+
+3) IF문 사용 - IF(조건문, TRUE, FALSE)
+    SELECT ANIMAL_TYPE
+         , IF(NAME IS NULL, 'No name', NAME) AS NAME
          , SEX_UPON_INTAKE
       FROM ANIMAL_INS 
   ORDER BY ANIMAL_ID      
