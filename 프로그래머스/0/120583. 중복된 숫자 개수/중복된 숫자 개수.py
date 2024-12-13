0d2ca8270807e53462a9d964eda70bfa446ad606
sigorzav <124122137+sigorzav@users.noreply.github.com>
2024-12-13 10:22:28 +0900

    Update 중복된 숫자 개수.py

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
@@ -6,3 +6,20 @@ def solution(array, n):
             answer += 1
     
     return answer
+
+def solution(array, n):  
+    return array.count(n)
+
+static PyObject *
+list_count(PyListObject *self, PyObject *value)
+{
+    Py_ssize_t count = 0;
+    Py_ssize_t i;
+
+    for (i = 0; i < PyList_GET_SIZE(self); i++) {
+        if (PyObject_RichCompareBool(PyList_GET_ITEM(self, i), value, Py_EQ) > 0) {
+            count++;
+        }
+    }
+    return PyLong_FromSsize_t(count);
+}
