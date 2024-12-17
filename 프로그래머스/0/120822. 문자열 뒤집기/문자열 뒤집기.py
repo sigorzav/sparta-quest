71aa8486cc03cb4a75bb1e1feeed6310762b217f
sigorzav <124122137+sigorzav@users.noreply.github.com>
2024-12-17 09:52:59 +0900

    Update 문자열 뒤집기.py

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -2,3 +2,22 @@ def solution(my_string):
     answer = list(my_string)
     answer.reverse()
     return ''.join(answer)
+
+# etc 1.
+def solution(my_string):
+    return my_string[::-1]
+
+# etc 2.
+def solution(my_string):
+    answer = ''
+
+    for i in range(len(my_string)-1, -1, -1) :
+        answer += my_string[i]
+    return answer
+
+# etc 3.
+def solution(my_string):
+    answer = ''
+    for c in my_string:
+        answer= c+answer
+    return answer
