704c2b1857db38336a6a46722844e9c66a9e9359
sigorzav <124122137+sigorzav@users.noreply.github.com>
2025-01-08 09:59:22 +0900

    Update 피자 나눠 먹기 （3）.py

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -8,3 +8,9 @@ def solution(slice, n):
             break
     
     return answer
+
+# 다른 사람의 풀이 [해석]
+# (n//slice)일 경우 n=slice 일 때 결과값이 기댓값보다 무조건 1 높게 나오기 때문에 -1. 
+# 그리고 뒤에 +1을 해주는 이유는 n-1//slice<1일 때 결과값이 0이 되는걸 방지하기 위함
+def solution(slice, n):
+    return ((n - 1) // slice) + 1
