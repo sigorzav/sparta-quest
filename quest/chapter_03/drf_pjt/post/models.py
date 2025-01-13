f026397c0a3ac2c0762926d7e0acd658b865ac0d
Mosekim <kimmose1992@gmail.com>
2025-01-14 02:21:13 +0900

    post 모델 - 댓글, 좋아요 추가

--- quest/chapter_03/drf_pjt/post/models.py
+++ quest/chapter_03/drf_pjt/post/models.py
@@ -7,8 +7,14 @@ class Post(TimestampModel):
     title       = models.CharField(max_length=50)
     content     = models.TextField()
     post_img    = models.ImageField(blank=True, upload_to='post_imgs/')
+    likes       = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="likes", blank=True)
     
     def get_image_url(self):
         if self.post_img:
             return self.post_img.url
         return '/static/common/images/default.jpg'
+    
+class Comment(TimestampModel):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
