a7fce7010e39f6a729c4608ed86165f65f510b75
Mosekim <kimmose1992@gmail.com>
2025-01-13 15:50:18 +0900

    1. 초기화면 설정 index > accounts/login 2. 템플릿 구조 설정 > base (navbar, footer)

--- quest/chapter_03/mtv_pjt/accounts/urls.py
+++ quest/chapter_03/mtv_pjt/accounts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
 app_name = "accounts"
 
-urlpatterns = []
+urlpatterns = [
+    path("login/", views.login, name="login"),
+    
+]
--- quest/chapter_03/mtv_pjt/mtv_pjt/urls.py
+++ quest/chapter_03/mtv_pjt/mtv_pjt/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.index),
     path("accounts/", include("accounts.urls")),
     path("blog/", include("blog.urls")),
 ]
