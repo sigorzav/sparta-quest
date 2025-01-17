cf19d08ee67c677cbc12df17a01341f37b3a8fcd
Mosekim <kimmose1992@gmail.com>
2025-01-17 17:52:22 +0900

    Comment, Tag 추가

--- quest/chapter_03/django_pjt/post/models.py
+++ quest/chapter_03/django_pjt/post/models.py
@@ -9,8 +9,18 @@ class Post(TimestampModel):
     post_img    = models.ImageField(blank=True, upload_to='post_imgs/')
     views       = models.IntegerField(default=0)
     likes       = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="likes", blank=True)
+    tags        = models.ManyToManyField('Tag', related_name="tags", blank=True)
     
     def get_image_url(self):
         if self.post_img:
             return self.post_img.url
         return '/static/common/images/default.jpg'
+    
+class Comment(models.Model):
+    author      = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments")
+    post        = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    content     = models.TextField()
+    
+class Tag(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="tags")
+    tag = models.CharField(max_length=20)
