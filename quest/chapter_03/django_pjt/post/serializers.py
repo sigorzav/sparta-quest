39a6383912b668ab5809c2718c9e6b9337c07b9e
Mosekim <kimmose1992@gmail.com>
2025-01-20 00:34:04 +0900

    댓글 삭제

--- quest/chapter_03/django_pjt/post/serializers.py
+++ quest/chapter_03/django_pjt/post/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.utils.dateformat import format
 from .models import Post, Comment
 
 class PostSerializers(serializers.ModelSerializer):
@@ -7,7 +8,25 @@ class PostSerializers(serializers.ModelSerializer):
         fields = "__all__"
         
 class CommentSerializers(serializers.ModelSerializer):
+    # 사용자 or 포맷팅 필드 추가
+    # SerializerMethodField :: 읽기 전용 필드
+    user_details = serializers.SerializerMethodField()
+    formatted_date = serializers.SerializerMethodField()
+    
     class Meta:
         model = Comment
-        fields = "__all__"
+        fields = ['id', 'content', 'author', 'post', 'user_details', 'formatted_date']
         read_only_fields = ("author", "post",)
+
+    # 직렬화 실행 시점에 메소드를 호출하여, 값을 채워줌
+    def get_user_details(self, obj):
+        return {
+            "nickname": obj.author.nickname,
+            "profile_img": obj.author.profile_img.url
+        }
+
+    def get_formatted_date(self, obj):
+        return {
+            'created_at': format(obj.created_at, 'Y-m-d H:i:s'),
+            'updated_at': format(obj.updated_at, 'Y-m-d H:i:s')
+        }
